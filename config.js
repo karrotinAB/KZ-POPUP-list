d62667d5f75858b6acdc2e48aeaad72885c0bf4e
karrotinAB <klee4297.artbox@gmail.com>
2026-08-12 20:12:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ⚠️ Supabase 프로젝트를 만든 뒤, 아래 두 값만 채워주세요.
 // Supabase 대시보드 → Project Settings → API 에서 확인할 수 있어요.
 const SUPABASE_URL = "https://rgcceftfkjzijmwsvhbc.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnY2NlZnRma2p6aWptd3N2aGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1MTEwMjAsImV4cCI6MjEwMjA4NzAyMH0.OBUdzt-P-BWetS0Frfor2eb6cVT-NAmNxhMpZKc27qI";
+const SUPABASE_ANON_KEY = "sb_publishable_u6sXintWhiSA9hEhiei_Ew_ezokQfHp";
